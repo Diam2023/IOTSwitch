200325958d00ce11324921797a91b340d2c9bf35
Diam2023 <monoliths-uni@outlook.com>
2024-01-17 14:01:15 +0800

    Completed json

--- main/inc/AppConfig.h
+++ main/inc/AppConfig.h
@@ -5,8 +5,79 @@
 #ifndef IOTSWITCH_APPCONFIG_H
 #define IOTSWITCH_APPCONFIG_H
 
+#include <string>
+#include "ArduinoJson/Document/StaticJsonDocument.hpp"
 
 
+class AppConfig {
+private:
+    std::string configVersion;
+
+    int64_t touchpadNotifyThreshold{};
+
+    int64_t touchpadTouchLimitThreshold{};
+
+    std::string mqttHost;
+    std::string mqttPort;
+
+    ArduinoJson::StaticJsonDocument<1024> configJson;
+
+public:
+    void load();
+
+    void write();
+
+
+    template<typename S>
+    void loadJsonConfig(S &&);
+
+    template<typename S>
+    void writeJsonConfig(S &&);
+
+    AppConfig() = default;
+
+public:
+    [[nodiscard]] const std::string &getConfigVersion() const {
+        return configVersion;
+    }
+
+    void setConfigVersion(const std::string &configVersion_) {
+        AppConfig::configVersion = configVersion_;
+    }
+
+    [[nodiscard]] int64_t getTouchpadNotifyThreshold() const {
+        return touchpadNotifyThreshold;
+    }
+
+    void setTouchpadNotifyThreshold(int64_t touchpadNotifyThreshold_) {
+        AppConfig::touchpadNotifyThreshold = touchpadNotifyThreshold_;
+    }
+
+    [[nodiscard]] int64_t getTouchpadTouchLimitThreshold() const {
+        return touchpadTouchLimitThreshold;
+    }
+
+    void setTouchpadTouchLimitThreshold(int64_t touchpadTouchLimitThreshold_) {
+        AppConfig::touchpadTouchLimitThreshold = touchpadTouchLimitThreshold_;
+    }
+
+    [[nodiscard]] const std::string &getMqttHost() const {
+        return mqttHost;
+    }
+
+    void setMqttHost(const std::string &mqttHost_) {
+        AppConfig::mqttHost = mqttHost_;
+    }
+
+    [[nodiscard]] const std::string &getMqttPort() const {
+        return mqttPort;
+    }
+
+    void setMqttPort(const std::string &mqttPort_) {
+        AppConfig::mqttPort = mqttPort_;
+    }
+
+};
 
 
 #endif //IOTSWITCH_APPCONFIG_H
--- main/src/AppConfig.cc
+++ main/src/AppConfig.cc
@@ -3,5 +3,78 @@
 //
 #include "AppConfig.h"
 
+#include "esp_log.h"
+#include <fstream>
+#include <sstream>
+#include "Common.h"
+#include <ArduinoJson.hpp>
 
+static const char *TAG = "AppConfig";
 
+void AppConfig::load() {
+    try {
+        ESP_LOGD(TAG, "Load config from file");
+        std::ifstream ifs(g_pConfigFilePath, std::ifstream::in);
+        std::stringstream buffer;
+        buffer << ifs.rdbuf();
+        std::string dt(buffer.str());
+        loadJsonConfig(dt);
+    }
+    catch (const std::exception &e) {
+        ESP_LOGE(TAG, "Error For Decode JSON: %s", e.what());
+    }
+}
+
+template<typename S>
+void AppConfig::loadJsonConfig(S &&stream) {
+    configJson.clear();
+    auto error = ArduinoJson::deserializeJson(configJson, stream);
+
+    if (error) {
+        // Throw A Error For Json Read
+        esp_log_write(ESP_LOG_ERROR, TAG, "%s", error.c_str());
+    }
+
+    if (!configJson["configVersion"].isNull()) {
+        this->setConfigVersion(configJson["configVersion"]);
+    }
+    if (!configJson["touchpad"]["notifyThreshold"].isNull()) {
+        this->setTouchpadNotifyThreshold(configJson["touchpad"]["notifyThreshold"]);
+    }
+    if (!configJson["touchpad"]["touchLimitThreshold"].isNull()) {
+        this->setTouchpadTouchLimitThreshold(configJson["touchpad"]["touchLimitThreshold"]);
+    }
+    if (!configJson["mqtt"]["host"].isNull()) {
+        this->setMqttHost(configJson["mqtt"]["host"]);
+    }
+    if (!configJson["mqtt"]["port"].isNull()) {
+        this->setMqttPort(configJson["mqtt"]["port"]);
+    }
+}
+
+void AppConfig::write() {
+
+    try {
+
+        std::ofstream ofs(g_pConfigFilePath, std::ofstream::out);
+        writeJsonConfig(ofs);
+        ESP_LOGD(TAG, "write successful!");
+    } catch (const std::exception &e) {
+        ESP_LOGD(TAG, "Error for write Config file: %s", e.what());
+    }
+}
+
+template<typename S>
+void AppConfig::writeJsonConfig(S &&stream) {
+
+    configJson.clear();
+
+    configJson["configVersion"] = this->getConfigVersion();
+    configJson["touchpad"]["notifyThreshold"] = this->getTouchpadNotifyThreshold();
+    configJson["touchpad"]["touchLimitThreshold"] = this->getTouchpadTouchLimitThreshold();
+    configJson["mqtt"]["host"] = this->getMqttHost();
+    configJson["mqtt"]["port"] = this->getMqttPort();
+
+    // encode This Json
+    ArduinoJson::serializeJson(configJson, stream);
+}
