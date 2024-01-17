6cc1125be7654fe4ba6e34c42d525aac5df84365
Diam2023 <monoliths-uni@outlook.com>
2024-01-17 19:43:17 +0800

    TemperatureSensor Prepare

---
+++ main/inc/TemperatureSensore.h
@@ -0,0 +1,16 @@
+//
+// Created by diam on 24-1-17.
+//
+
+#ifndef IOTSWITCH_TEMPERATURESENSORE_H
+#define IOTSWITCH_TEMPERATURESENSORE_H
+
+
+
+class TemperatureSensore {
+
+};
+
+
+
+#endif //IOTSWITCH_TEMPERATURESENSORE_H
---
+++ main/src/TemperatureSensore.cc
@@ -0,0 +1,5 @@
+//
+// Created by diam on 24-1-17.
+//
+
+#include "TemperatureSensore.h"
