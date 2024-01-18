aecc16b1ace21852c2a4a626b7c9e9206f66a7ff
Diam2023 <monoliths-uni@outlook.com>
2024-01-19 00:04:12 +0800

    Added Warning message when Height temperature

--- main/src/TemperatureSensor.cc
+++ main/src/TemperatureSensor.cc
@@ -6,17 +6,28 @@
 
 #include "AppConfig.h"
 #include "AppBuzzer.h"
+#include "MqttClient.h"
 
 #include <esp_log.h>
+#include <string>
+#include <sstream>
 
 static const char *TAG = "TEMPERATURE";
 
 // TODO prepare a mutex
 void TemperatureSensor::onListener(const unsigned long &d) {
-
     if (d >= temperatureThreshold) {
-        AppBuzzer::getInstance().beepMicroTimeSync({5, 30});
-        ESP_LOGE(TAG, "TEMPERATURE HEIGHT!!! %lu mV", d);
+        static int counter = 0;
+        if (counter++ >= 5) {
+            AppBuzzer::getInstance().beepMicroTimeSync({5, 30});
+            static std::stringstream ss;
+            ss.clear();
+            ss << "TEMPERATURE HEIGHT: " << d << "mV";
+            std::string msg = ss.str();
+            MqttClientManager::getClient()->publishWarning(msg);
+            ESP_LOGE(TAG, "TEMPERATURE HEIGHT!!! %lu mV", d);
+            counter = 0;
+        }
     }
 }
 
